6bd62b13c45e07806b6933d44e6616c72158f352
Tanya Chamala <tanya.chamala@slu.edu>
2024-11-14 09:32:39 -0600

    code changed

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from base import app
 
+port_number = 5671
 
-if __name__ == '__main__':
-    app.run(threaded=True, debug=True, host='localhost', port=2807)
+if __name__ == "__main__":
+    app.run(threaded=True, host='localhost', port=port_number)
 
+
+# python flask orm libs
+# pip install flask
+# pip install pymysql
+# pip install sqlalchemy
+# pip install flask-sqlalchemy
