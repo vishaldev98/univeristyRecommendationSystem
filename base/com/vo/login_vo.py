1f8974dc6406b3235ba3327855a520551833f903
Tanya Chamala <tanya.chamala@slu.edu>
2024-11-14 09:31:42 -0600

    code changed

--- base/com/vo/login_vo.py
+++ base/com/vo/login_vo.py
@@ -4,11 +4,11 @@ from base import db
 class LoginVO(db.Model):
     __tablename__ = 'login_table'
     login_id = db.Column('login_id', db.Integer, primary_key=True, autoincrement=True)
-    login_username = db.Column('login_username', db.String(255), nullable=False)
-    login_password = db.Column('login_password', db.String(255), nullable=False)
-    login_role = db.Column('login_role', db.String(255), nullable=False)
-    login_status = db.Column('login_status', db.String(255), nullable=False)
-    login_secretkey = db.Column('login_secretkey', db.String(255), nullable=False)
+    login_username = db.Column('login_username', db.String(100), nullable=False)
+    login_password = db.Column('login_password', db.String(100), nullable=False)
+    login_role = db.Column('login_role', db.String(100), nullable=False)
+    login_status = db.Column('login_status', db.String(100), nullable=False)
+    login_secretkey = db.Column('login_secretkey', db.String(100), nullable=False)
 
     def as_dict(self):
         return {
@@ -21,3 +21,4 @@ class LoginVO(db.Model):
         }
 
 
+db.create_all()
